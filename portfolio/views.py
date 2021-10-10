19b3079c796e58b4b8e66ef0c123fea63e8a59e4
Isaac Duke <ird2dpj@virginia.edu>
2021-10-09 20:04:14 -0400

    fixing images

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,11 +8,11 @@ def project_index(request):
     context = {
         'projects': projects
     }
-    return render(request, 'project_index.html', context)
+    return render(request, 'portfolio/project_index.html', context)
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     context = {
         'project':project
     }
-    return render(request, 'project_detail.html', context)
+    return render(request, 'portfolio/project_detail.html', context)
