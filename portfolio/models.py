632001930e8ce5da9f26bc93625af738faf8eae0
Isaac Duke <ird2dpj@virginia.edu>
2021-10-09 20:00:22 -0400

    initial work!

---
+++ portfolio/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.FilePathField(path='/static/portfolio/images')
+
+
+    def __str__(self):
+        return f'{self.title}: {self.description[:25]}...'
