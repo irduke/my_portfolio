a050cc184c4d6482b4ec53a678aaade3090a90d5
Isaac Duke <ird2dpj@virginia.edu>
2021-10-11 17:14:24 -0400

    added a navbar and styling

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.project_index, name='project_index'),
     path('<int:pk>/', views.project_detail, name='project_detail'),
+    path('contactinfo/', views.contact_information, name='contact_information'),
+    path('aboutme/', views.about_me, name='about_me'),
 ]
