c4b99a3b159f680832c09ace349261311cd8037d
Isaac Duke <ird2dpj@virginia.edu>
2021-10-11 14:27:18 -0400

    fixed image issue

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.FilePathField(path='/static/portfolio/images')
+    image = models.ImageField(upload_to='images/')
 
 
     def __str__(self):
